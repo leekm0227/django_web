f7c8c2f5c9dafb47c13513d4022a8acbc10fdd0d
lgm <leegm@valofe.com>
2019-05-03 12:29:42 +0900

    add

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,6 @@ from api import views
 urlpatterns = [
     path('auth/register', include('rest_auth.registration.urls')),
     path('auth/', include('rest_auth.urls')),
-    path('articles/', views.ArticleList.as_view()),
+    path('articles', views.ArticleList.as_view()),
     path('articles/<int:pk>', views.ArticleDetail.as_view()),
 ]
---
+++ fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import env, run
+
+env.user = "leekm0227"
+env.hosts = ["35.239.104.137"]
+env.password = "0000"
+PROJECT_NAME = "django_web"
+PROJECT_DIR = '/home/{}/{}'.format(env.user, PROJECT_NAME)
+
+
+def deploy():
+    run('date')
+    run('uwsgi --stop ' + PROJECT_DIR + '/uwsgi.pid')
+    run('git --git-dir=' + PROJECT_DIR + '/.git pull')
+    run('uwsgi --ini ' + PROJECT_DIR + '/uwsgi.ini')
